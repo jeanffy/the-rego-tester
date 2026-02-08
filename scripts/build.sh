38bb12b72f7dafada985b88ae5a631c174d85cfd
jeanffy <26540464+jeanffy@users.noreply.github.com>
2026-02-08 18:25:06 +0100

    feat: added github action create release workflow

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,16 @@
 rm -rf dist
 
-echo "Building Linux amd64"
-GOOS=linux GOARCH=amd64 go build -o dist/regotest_linux_amd64 ./cmd/regotest
+build() {
+  argOS=$1
+  argARCH=$2
+  fileName="regotest_${argOS}_${argARCH}"
+  destFilePath="dist/$fileName"
+  echo "Building $argOS $argARCH -> $destFilePath"
+  GOOS=$argOS GOARCH=$argARCH go build -o $destFilePath ./cmd/regotest
+  (cd dist && sha256sum $fileName > $fileName.sha256)
+}
 
-echo "Building Linux arm64"
-GOOS=linux GOARCH=arm64 go build -o dist/regotest_linux-arm64 ./cmd/regotest
-
-echo "Building macOS amd64"
-GOOS=darwin GOARCH=amd64 go build -o dist/regotest_macos_amd64 ./cmd/regotest
-
-echo "Building macOS arm64"
-GOOS=darwin GOARCH=arm64 go build -o dist/regotest_macos_arm64 ./cmd/regotest
+build linux amd64
+build linux arm64
+build darwin amd64
+build darwin arm64
